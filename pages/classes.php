bf4d6775faa2e0a97503256a1592e47dbd0ee1d1
AndrGud <andrey.gudiy@gmail.com>
2017-11-29 14:22:18 +0300

    МкСервис

--- pages/classes.php
+++ pages/classes.php
@@ -22,7 +22,7 @@ class Tools
 
 class Articles
 {
-    static function count_symbol($month)
+    static function count_symbol($month, $year)
     {
         try {
             global $host;
@@ -35,7 +35,7 @@ class Articles
             $pdo = Tools::connect($host, $user, $pass, $dbname);
             $sel = "SELECT name, elements 
                   FROM $table_name
-                  WHERE application_id=? AND type=? AND publish_up LIKE '%%-$month-%%'";
+                  WHERE application_id=? AND type=? AND publish_up LIKE '$year-$month-%%'";
             $ps = $pdo->prepare($sel);
             $ps->execute(array($application_id, $type));
             $long = 0;
@@ -43,14 +43,10 @@ class Articles
                 $obj = json_decode($row['elements']);
                 $text_count = $obj->{'3034c0b9-e036-4573-b3a8-c7353bd5b51e'}->{0}->value;
                 $text_count = strip_tags($text_count);
-                $ar=[' ', "\r\n", "\r", "\n", "<br>", "<br/>"];
-                $text_count = str_replace($ar,"", $text_count);
-                echo '<pre>';
-                print_r($text_count);
-                echo '</pre>';
+                $ar = [' ', "\r\n", "\r", "\n"];
+                $text_count = str_replace($ar, "", $text_count);
                 $long += iconv_strlen($text_count);
             }
-
             return $long;
         } catch (PDOException $e) {
             echo $e->getMessage();
@@ -71,6 +67,69 @@ class Articles
             $row = $ps->fetch();
             $month = $row['month'];
             return $month;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    static function getArticles($month, $year)
+    {
+        try {
+            global $host;
+            global $user;
+            global $pass;
+            global $dbname;
+            global $table_name;
+            global $type;
+            global $application_id;
+            $pdo = Tools::connect($host, $user, $pass, $dbname);
+            $num = 100;
+            $page = $_GET['page'];
+            $res = $pdo->query("SELECT COUNT(*) FROM $table_name");
+            $res->setFetchMode(PDO::FETCH_NUM);
+            $temp = $res->fetch();
+            $posts = $temp[0];
+            $total = (($posts - 1) / $num) + 1;
+            $total = intval($total);
+            $page = intval($page);
+            if (empty($page) || $page < 0) $page = 1;
+            if ($page > $total) $page = $total;
+            $start = $page * $num - $num;
+            $pdo = Tools::connect($host, $user, $pass, $dbname);
+            $ps = $pdo->prepare("select name, elements 
+                                from $table_name 
+                                WHERE application_id=? AND type=? AND publish_up LIKE '$year-$month-%%'
+                                LIMIT $start, $num");
+            $ps->execute(array($application_id, $type));
+            echo '<table class="table table-striped">';
+            echo '<tr><th>Название записи</th><th>Количество символов</th></tr>';
+            $ps->setFetchMode(PDO::FETCH_ASSOC);
+            $total_symbol = 0;
+            while ($row = $ps->fetch()) {
+                $obj = json_decode($row['elements']);
+                $text_count = $obj->{'3034c0b9-e036-4573-b3a8-c7353bd5b51e'}->{0}->value;
+                $text_count = strip_tags($text_count);
+                $ar = [' ', "\r\n", "\r", "\n"];
+                $text_count = str_replace($ar, "", $text_count);
+                $long_text = iconv_strlen($text_count);
+                $total_symbol = $total_symbol + $long_text;
+                echo '<tr><td>' . $row['name'] . '</td><td>' . $long_text . '</td></tr>';
+            }
+            echo '<tr><th><h4>Итого</h4></th><th><h4>' . $total_symbol . '</h4></th></tr>';
+            echo '</table>';
+
+            if ($page != 1) $pervpage = '<a href=table_month.php?mid=' . $month . '&yid=' . $year . '&page=1></a> | <a href=table_month.php?mid=' . $month . '&yid=' . $year . '&page=' . ($page - 1) . '></a> | ';
+            if ($page != $total) $nextpage = ' | <a href=table_month.php?mid=' . $month . '&yid=' . $year . '&page=' . ($page + 1) . '></a> | <a href=table_month.php?mid=' . $month . '&yid=' . $year . '&page=' . $total . '></a>';
+
+            if ($page - 1 > 0) $page1left = '<a href=table_month.php?mid=' . $month . '&yid=' . $year . '&page=' . ($page - 1) . '>' . ($page - 1) . '</a> | ';
+            if ($page + 1 <= $total) $page1right = ' | <a href=table_month.php?mid=' . $month . '&yid=' . $year . '&page=' . ($page + 1) . '>' . ($page + 1) . '</a>';
+
+            if ($total > 1) {
+                echo "<div class='text-center'>";
+                echo $pervpage . $page1left . '<b>' . $page . '</b>' . $page1right . $nextpage;
+                echo "</div>";
+            }
         } catch (PDOException $e) {
             echo $e->getMessage();
             return false;
--- pages/select_month.php
+++ pages/select_month.php
@@ -1,34 +1,40 @@
-<h2>Год 2017</h2>
 <?php
 echo '<form action="index.php" method="post">
     <select name="number_month" class="select_montch">';
-    $pdo=Tools::connect($host, $user, $pass, $dbname);
-    $sel='select * from month';
-    $ps=$pdo->prepare($sel);
-    $ps->execute();
-    while($row=$ps->fetch()){
-        echo '<option value="' . $row[id] . '">' . $row[month] . '</option>';
-    }
+$pdo = Tools::connect($host, $user, $pass, $dbname);
+$sel = 'select * from month';
+$ps = $pdo->prepare($sel);
+$ps->execute();
+while ($row = $ps->fetch()) {
+    echo '<option value="' . $row[id] . '">' . $row[month] . '</option>';
+}
 echo
-    '</select>
+'</select>
+    <select name="number_year">
+        <option value="2016">2016</option>
+        <option value="2017">2017</option>
+        <option value="2018">2018</option>
+    </select>
     <input type="submit" name="sub1" value="Посмотреть" class="btn btn-success">
 </form>';
 
-if(isset($_POST['sub1'])) {
-    $month=$_POST['number_month'];
+if (isset($_POST['sub1'])) {
+    $month = $_POST['number_month'];
     /*echo '<table class="table table-striped">
             <tr><th>Месц</th><th>Количество символов</th></tr>
             <tr><td>' . Articles::getMonthName($month). '</td><td>' .Articles::count_symbol($month). '</td></tr>
         </table>';*/
+    $year = $_POST['number_year'];
+    echo '<h2>Год ' . $year . '</h2>';
     echo '<table class="table table-striped">';
     echo '<tr><th>Месц</th><th>Количество символов</th></tr>';
-    $pdo=Tools::connect($host, $user, $pass, $dbname);
-    $sel='select * from month';
-    $ps=$pdo->prepare($sel);
+    $pdo = Tools::connect($host, $user, $pass, $dbname);
+    $sel = 'select * from month';
+    $ps = $pdo->prepare($sel);
     $ps->execute();
-    while($row=$ps->fetch()){
-        $id=$row['id'];
-        echo '<tr><td>'.$row['month'] . '</td><td><a href="page/table_month.php">'.Articles::count_symbol($id).'</a></td></tr>';
+    while ($row = $ps->fetch()) {
+        $id = $row['id'];
+        echo '<tr><td>' . $row['month'] . '</td><td><a href="pages/table_month.php?mid=' . $id . '&yid=' . $year . '">' . Articles::count_symbol($id, $year) . '</a></td></tr>';
     }
     echo '</table>';
 }
