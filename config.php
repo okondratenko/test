63a9b92ce830dcbfc48f7c35d61a684dd98ecb7c
AndrGud <andrey.gudiy@gmail.com>
2017-12-06 19:03:42 +0300

    spl_autoload_register(function ($class_name) {
        include_once PATH.'/classes/' . $class_name . '.php';
    });

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-spl_autoload_register(function ($class_name) {
-    include_once $_SERVER['DOCUMENT_ROOT'].'/joomla_count_symbols/classes/' . $class_name . '.php';
-});
 define("PATH", __DIR__);
+spl_autoload_register(function ($class_name) {
+    include_once PATH.'/classes/' . $class_name . '.php';
+});
 $ini_array      = parse_ini_file("config.ini", true);
 $pdo = Connect::pdo($ini_array['PDO']['host'], $ini_array['PDO']['user'], $ini_array['PDO']['pass'], $ini_array['PDO']['dbname']);
 ?>
